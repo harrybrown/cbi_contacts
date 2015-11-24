dedfec96914a5897ac7f299ee830cda590f47bde
Leroy Brown <harry@-rexdesigns.com>
2015-11-24 08:59:14 -0600

    redump current seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,5 +6,12 @@ Contact.create!([
   {id: 5, first_name: "Woody", last_name: "Harrelson", email: "mickeynmallory@aol.com", phone: "(666) 538-0181", created_at: "2015-11-23 02:18:52", updated_at: "2015-11-23 02:18:52"},
   {id: 6, first_name: "James", last_name: "Bond", email: "007@compuserve.net", phone: "(007) 838-9292", created_at: "2015-11-23 21:22:00", updated_at: "2015-11-23 21:22:00"},
   {id: 7, first_name: "Jiminy", last_name: "Cricket", email: "jim@disney.com", phone: "(312) 928-1912", created_at: "2015-11-23 21:27:24", updated_at: "2015-11-23 21:27:24"},
-  {id: 8, first_name: "Sirius", last_name: "Black", email: "sirius@hogwarts.com", phone: "", created_at: "2015-11-23 21:29:59", updated_at: "2015-11-23 21:29:59"}
+  {id: 8, first_name: "Sirius", last_name: "Black", email: "sirius@hogwarts.com", phone: "", created_at: "2015-11-23 21:29:59", updated_at: "2015-11-23 21:29:59"},
+  {id: 9, first_name: "Jean Luc", last_name: "Picard", email: "picard@starfleet.com", phone: "(219) 844-0658", created_at: "2015-11-24 02:42:34", updated_at: "2015-11-24 02:42:34"},
+  {id: 10, first_name: "Atticus", last_name: "Finch", email: "finch@mockingbird.net", phone: "", created_at: "2015-11-24 02:44:04", updated_at: "2015-11-24 02:44:04"},
+  {id: 11, first_name: "Doctor", last_name: "Who", email: "time-travel@tardis.com", phone: "UNLISTED", created_at: "2015-11-24 02:48:45", updated_at: "2015-11-24 02:48:45"},
+  {id: 12, first_name: "Bob", last_name: "Cratchett", email: "bobby@scrooge-inc.com", phone: "NONE", created_at: "2015-11-24 02:53:01", updated_at: "2015-11-24 02:53:01"},
+  {id: 13, first_name: "Glen", last_name: "Quagmire", email: "giggity@quahog.com", phone: "(888) HOT-DATE", created_at: "2015-11-24 02:57:46", updated_at: "2015-11-24 02:57:46"},
+  {id: 14, first_name: "Bruce", last_name: "Wayne", email: "ceo@wayne-enterprises.com", phone: "(911) 626-4300", created_at: "2015-11-24 03:06:00", updated_at: "2015-11-24 03:06:00"},
+  {id: 15, first_name: "Jack", last_name: "Skellington", email: "jacskelton@hotmail.com", phone: "(666) 676-7262", created_at: "2015-11-24 03:06:41", updated_at: "2015-11-24 03:06:41"}
 ])
