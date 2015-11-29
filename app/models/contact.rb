2589f3bb9a668abbdcebe1eca168d9fcccc040f1
Leroy Brown <harry@-rexdesigns.com>
2015-11-28 20:49:32 -0600

    removed rspec for reinstall

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,4 +1,7 @@
 class Contact < ActiveRecord::Base
+
+  attr_accessor :first_name, :last_name, :email, :phone
+
   validates :first_name, :presence => true
   validates :last_name, :presence => true
 
